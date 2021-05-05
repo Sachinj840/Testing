9764c9f53c51ccb1b45405591b76c41aaa8dd036
sachin <sachinj@leotechnosoft.net>
2021-05-05 10:15:27 +0530

    new files

---
+++ New folder/RouteConfig.cs
@@ -0,0 +1,17 @@
+﻿using DotNetNuke.Web.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnCall.NurseVitalsMonitoring.NurseVitalsMonitoringDashboard
+{
+    public class RouteConfig
+    {
+        public void RegisterRoutes(IMapRoute mapRouteManager)
+
+        {
+            mapRouteManager.MapRoute("NurseVitalsMonitoringDashboard", "NurseVitalsMonitoringDashboard", "{controller}/{action}", new[] { "OnCall.NurseVitalsMonitoring.NurseVitalsMonitoringDashboard.Controllers" });
+        }
+    }
+}
